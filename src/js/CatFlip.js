07fd4ae3d54d9080b4d5a015a7ddd0d3f858b8d2
barsich <barsicchan@gmail.com>
2021-07-07 17:30:06 +0500

    dom1 hw edited

--- src/js/CatFlip.js
+++ src/js/CatFlip.js
@@ -1,5 +1,24 @@
+/* eslint-disable no-plusplus */
 export default class CatFlip {
   constructor() {
+    this.fieldSize = 4;
+    // this.cells = Array.from(document.querySelectorAll('.cell'));
+    this.field = document.querySelector('.field');
+    this.cells = null;
+    this.previousIndex = null;
+  }
+
+  createField() {
+    for (let i = 0; i < this.fieldSize; i++) {
+      const col = document.createElement('div');
+      col.classList.add('col');
+      this.field.appendChild(col);
+      for (let j = 0; j < this.fieldSize; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        col.appendChild(cell);
+      }
+    }
     this.cells = Array.from(document.querySelectorAll('.cell'));
   }
 
@@ -7,7 +26,12 @@ export default class CatFlip {
     if (document.querySelector('.cat')) {
       document.querySelector('.cat').remove();
     }
-    const randomCell = this.cells[Math.floor(Math.random() * this.cells.length)];
+    let randomIndex = Math.floor(Math.random() * this.cells.length);
+    while (randomIndex === this.previousIndex) {
+      randomIndex = Math.floor(Math.random() * this.cells.length);
+    }
+    const randomCell = this.cells[randomIndex];
+    this.previousIndex = randomIndex;
     const cat = document.createElement('div');
     cat.classList.add('cat');
     randomCell.appendChild(cat);
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import CatFlip from './CatFlip';
 
 document.addEventListener('DOMContentLoaded', () => {
   const catFlip = new CatFlip();
+  catFlip.createField();
   const flip = catFlip.flip.bind(catFlip);
   setInterval(flip, 2000);
 });
